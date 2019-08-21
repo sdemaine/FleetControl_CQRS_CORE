03d18a5c1ee7f7f7b1bbeb5e2bbf6f284f76adb0
Stephen Demaine <stephendemaine@live.com>
2019-08-21 11:25:39 -0400

    added billing code validation

--- FleetControl.Application.Commands/Customers/UpdateCustomer/UpdateFleetCustomerCommandValidator.cs
+++ FleetControl.Application.Commands/Customers/UpdateCustomer/UpdateFleetCustomerCommandValidator.cs
@@ -1,5 +1,5 @@
 ﻿using FluentValidation;
-using FluentValidation.Validators;
+using System.Collections.Generic;
 
 namespace FleetControl.Application.Commands.UpdateCustomer
 {
@@ -8,6 +8,20 @@ namespace FleetControl.Application.Commands.UpdateCustomer
         public UpdateCustomerCommandValidator()
         {
             RuleFor(x => x.Customer.CustomerName).MaximumLength(50).NotEmpty();
+            RuleFor(x => x.Customer.BillingCode).Custom((billingCode, context) => {
+
+
+                if (!IsValidBillingCode(billingCode))
+                {
+                    context.AddFailure("The billing code is not valid");
+                }
+
+                
+                //if (list.Count > 10)
+                //{
+                //    context.AddFailure("The list must contain 10 items or fewer");
+                //}
+            });
 
 
             //RuleFor(c => c.PostalCode).Matches(@"^\d{4}$")
@@ -24,5 +38,60 @@ namespace FleetControl.Application.Commands.UpdateCustomer
         //{
         //    return model.Phone.StartsWith("07") || model.Fax.StartsWith("07");
         //}
+
+        public enum Group
+        {
+            A,
+            B,
+            C
+        }
+
+        public static List<string> BillingCodes = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "M", "N", "P", "Q", "S", "T", "W" };
+
+
+        bool IsValidBillingCode(string billingCode)
+        {
+            if (billingCode == null || billingCode == string.Empty) return false;
+
+            if (BillingCodes.Contains(billingCode.ToUpper()))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //public static class BillingCode
+        //{
+        //    public const string AIRBORNE_TWICE_A_MONTH = "A";
+        //    public const string TWICE_A_MONTH = "B";
+        //    public const string COMMERCIAL_WEEKLY_BILLING = "C";
+        //    public const string WEEKLY_WET_HOSING = "E";
+        //    public const string WET_HOSING = "D";
+        //    public const string BI_MONTHLY_WET_HOSING = "F";
+        //    public const string MONTHLY_WET_HOSING = "G";
+        //    public const string HOUSING_AUTHORITY = "H";
+        //    public const string IGNORE = "I";
+        //    public const string MONTHLY = "M";
+        //    public const string NONE = "N";
+        //    public const string POST_OFFICE = "P";
+        //    public const string CONTINENTAL_CHELSEA = "Q";
+        //    public const string SWITCH_CYCLES = "S";
+        //    public const string END_OF_MONTH_TOTAL_ACTIVITY = "T";
+        //    public const string WEEKLY_NYC_BILLING = "W";
+
+        bool IsValidResourceGroup(string group)
+        {
+            //foreach (var c in Enum.GetValues(typeof(BillingCode)))
+            //{
+            //    if (string.Equals(group.Trim(), $"{c}", StringComparison.CurrentCultureIgnoreCase))
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
+
+            return false;
+        }
     }
 }
---
+++ FleetControl.Core/Enums/BillingCode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetControl.Core.Enums
+{
+    //public static class BillingCode
+    //{
+    //    public const string AIRBORNE_TWICE_A_MONTH = "A";
+    //    public const string TWICE_A_MONTH = "B";
+    //    public const string COMMERCIAL_WEEKLY_BILLING = "C";
+    //    public const string WEEKLY_WET_HOSING = "E";
+    //    public const string WET_HOSING = "D";
+    //    public const string BI_MONTHLY_WET_HOSING = "F";
+    //    public const string MONTHLY_WET_HOSING = "G";
+    //    public const string HOUSING_AUTHORITY = "H";
+    //    public const string IGNORE = "I";
+    //    public const string MONTHLY = "M";
+    //    public const string NONE = "N";
+    //    public const string POST_OFFICE = "P";
+    //    public const string CONTINENTAL_CHELSEA = "Q";
+    //    public const string SWITCH_CYCLES = "S";
+    //    public const string END_OF_MONTH_TOTAL_ACTIVITY = "T";
+    //    public const string WEEKLY_NYC_BILLING = "W";
+
+    //    public static void GetList()
+    //    {
+    //        //FieldInfo[] fields = typeof(BillingCode).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+    //        PropertyInfo[] fields = typeof(BillingCode).GetProperties(BindingFlags.Static);
+    //        foreach (var field in fields)
+    //        {
+    //            var field2 = field;
+    //            // do something
+    //        }
+    //    }
+        
+    //}
+}
